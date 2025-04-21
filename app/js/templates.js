998362b164d0dbc36726445854727916171bb376
Ahmedelmalki <elmalkiahmed02@gmail.com>
2025-04-21 18:40:25 +0100

    working on the graph

--- app/js/templates.js
+++ app/js/templates.js
@@ -7,6 +7,8 @@ export async function greetingBox(token, query) {
   const user = data.data.data.user[0];
   const firstName = user.firstName;
   const lastName = user.lastName;
+  console.log('firstName ====>',firstName);
+  
   greetingDiv.innerText = `Welcome, ${firstName} ${lastName}`;
   app.appendChild(greetingDiv);
 }
@@ -21,11 +23,11 @@ export async function levelBox(token, query) {
   levelDiv.style.border = "2px solid black";
 
   levelDiv.innerHTML = /*html*/ `
-                          <div>level </div>
-                          <svg height="100" width="100">
-                            <circle r="45" cx="50" cy="50" fill="transparent" stroke="black" stroke-width="10" />
-                            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="20" fill="black">${level}</text>
-                          </svg>
+  <div>level</div>
+   <svg class="level-circle" viewBox="0 0 100 100" width="5em" height="5em" preserveAspectRatio="xMidYMid meet">
+    <circle r="45" cx="50" cy="50" fill="transparent" stroke="black" stroke-width="10" />
+    <text x="50" y="50" dominant-baseline="middle" text-anchor="middle" font-size="20" fill="black">${level}</text>
+  </svg>
     `;
   lb.appendChild(levelDiv);
 }
@@ -50,25 +52,24 @@ export async function auditBox(token, query) {
     <div class="audit-ratio">Audits ratio</div>
     <div class="box">
       <div>Done</div>
-    <div class="line1"> 
-      <svg height="50" width="294">
-        <line x1="5" y1="10" x2="${
-          doneWidth - 5
-        }" y2="10" style="stroke:red;stroke-width:12" />
-      </svg>
-      <div class="prs">${String(done)[0]}.${String(done)[1]}</div>
-    </div>
-    <div>Received</div>
-    <div class="line2"> 
-      <svg height="50" width="294">
-        <line x1="5" y1="10" x2="${
-          receivedWidth - 5
-        }" y2="10" style="stroke:red;stroke-width:12" />
-      </svg>
-      <div class="prs">${String(received)[0]}.${String(received)[1]}</div>
-    </div>
+      <div class="line-wrap">
+        <svg height="50" width="294">
+          <line x1="5" y1="10" x2="${doneWidth - 10}" y2="10" style="stroke:red;stroke-width:12" />
+        </svg>
+        <div class="prs">${String(done)[0]}.${String(done)[1]}</div>
+      </div>
+
+      <div>Received</div>
+      <div class="line-wrap">
+        <svg height="50" width="294">
+          <line x1="5" y1="10" x2="${receivedWidth - 10 }" y2="10" style="stroke:red;stroke-width:12" />
+        </svg>
+        <div class="prs">${String(received)[0]}.${String(received)[1]}</div>
+      </div>
+
       <div class="ratio">${ar}</div>
     </div>
+
     `;
   lb.appendChild(auditBox);
 }
@@ -76,7 +77,7 @@ export async function auditBox(token, query) {
 export async function xpBox(token, query) {
   const rb = document.querySelector(".right-boxes");
   const xpData = await fetchData(token, query);
-  console.log("xpData ============>", xpData.data.data.transaction);
+  // console.log("xpData ============>", xpData.data.data.transaction);
 
   const xpDiv = document.createElement("div");
   xpDiv.className = "xpDiv";
@@ -105,7 +106,7 @@ export async function xpBox(token, query) {
     const p = formatPath(t.path);
     pathSpan.innerText = `${p}`; // project/exam
     transaction_container.appendChild(pathSpan);
-    console.log("t.path ======>", t.path);
+    // console.log("t.path ======>", t.path);
 
     const amountSpan = document.createElement("span");
     amountSpan.className = "amountSpan";
